11a0afecaf1f5e5a92ced3c363371500224d61f6
guilhermedevel <guilherme.zarzur@gmail.com>
2023-02-21 12:33:52 -0300

    Update para categorias

--- src/main/java/com/guilherme/bookstore/service/CategoriaService.java
+++ src/main/java/com/guilherme/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.bookstore.domain.Categoria;
+import com.guilherme.bookstore.dtos.CategoriaDTO;
 import com.guilherme.bookstore.repositories.CategoriaRepository;
 import com.guilherme.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -33,5 +34,13 @@ public class CategoriaService {
 
 		obj.setId(null);
 		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+		
 	}
 }
