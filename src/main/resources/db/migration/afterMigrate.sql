931e344f626de84744e672cc0dc9701e12d7fd4e
guilhermedevel <guilherme.zarzur@gmail.com>
2023-03-07 15:58:55 -0300

    fix: including id in inserts of afterMigrate.sql, create and configure archive system.properties

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -1,9 +1,9 @@
-INSERT IGNORE INTO categoria (nome, descricao) VALUES ("Informática", "Livros de TI");
-INSERT IGNORE INTO categoria (nome, descricao) VALUES ("Ficção Científica", "Ficção Científica");
-INSERT IGNORE INTO categoria (nome, descricao) VALUES ("Biografias", "Livros de Biografia");
+INSERT IGNORE INTO categoria (id, nome, descricao) VALUES (1, "Informática", "Livros de TI");
+INSERT IGNORE INTO categoria (id, nome, descricao) VALUES (2, "Ficção Científica", "Ficção Científica");
+INSERT IGNORE INTO categoria (id, nome, descricao) VALUES (3, "Biografias", "Livros de Biografia");
 
-INSERT IGNORE INTO livro (titulo, nome_autor, texto, categoria_id) VALUES ("Clean Code", "Robert Martin", "Lorem ipsum", 1);
-INSERT IGNORE INTO livro (titulo, nome_autor, texto, categoria_id) VALUES ("Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", 1);
-INSERT IGNORE INTO livro (titulo, nome_autor, texto, categoria_id) VALUES ("The Time Machine", "H.G. Wells", "Lorem ipsum", 2);
-INSERT IGNORE INTO livro (titulo, nome_autor, texto, categoria_id) VALUES ("The War of the Worlds", "H.G. Wells", "Lorem ipsum", 2);
-INSERT IGNORE INTO livro (titulo, nome_autor, texto, categoria_id) VALUES ("I, Robot", "Isaac Asimov", "Lorem ipsum", 2);
+INSERT IGNORE INTO livro (id, titulo, nome_autor, texto, categoria_id) VALUES (1, "Clean Code", "Robert Martin", "Lorem ipsum", 1);
+INSERT IGNORE INTO livro (id, titulo, nome_autor, texto, categoria_id) VALUES (2, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", 1);
+INSERT IGNORE INTO livro (id, titulo, nome_autor, texto, categoria_id) VALUES (3, "The Time Machine", "H.G. Wells", "Lorem ipsum", 2);
+INSERT IGNORE INTO livro (id, titulo, nome_autor, texto, categoria_id) VALUES (4, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", 2);
+INSERT IGNORE INTO livro (id, titulo, nome_autor, texto, categoria_id) VALUES (5, "I, Robot", "Isaac Asimov", "Lorem ipsum", 2);
